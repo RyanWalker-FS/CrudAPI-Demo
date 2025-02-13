24f1a7378fe0e4027b1439e7c00da3d0318aef36
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-12 23:28:19 -0500

    update

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [students, setStudents] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState(null);
+  const API_BASE =
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:8000`
+      : process.env.REACT_APP_API_BASE;
+  let ignore = false;
+  useEffect(() => {
+    if (!ignore) {
+      getStudents();
+    }
+    return () => {
+      ignore = true;
+    };
+  });
+  const getStudents = async () => {
+    try {
+      await fetch(`${API_BASE}/students`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setStudents(data);
+        });
+    } catch (err) {
+      setError(error.message || "unexpected error");
+    } finally {
+      setloading(false);
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Students:</h1>
+          <ul>
+            <li>John</li>
+            <li>Paul</li>
+            <li>George</li>
+            <li>Ronny</li>
+          </ul>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
