a3ab3338ce1d88d706afb701a4d8a0352d500832
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-12 23:52:35 -0500

    app update

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,18 +1,35 @@
-import react, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
-import { set } from "mongoose";
 
 function App() {
-  const [students, setStudents] = useState([]);
-  const [loading, setloading] = useState(false);
-  const [error, setError] = useState(null);
   const API_BASE =
     process.env.NODE_ENV === "development"
       ? `http://localhost:8000`
       : process.env.REACT_APP_API_BASE;
+  let ignore = false;
+
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getStudents = async () => {
+    try {
+      await fetch(`${API_BASE}/students`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data fetched:", data);
+          setStudents(data);
+        });
+    } catch (err) {
+      console.error("error:", err);
+      setError(err.message || "unexpected error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    let ignore = false;
     if (!ignore) {
       getStudents();
     }
@@ -20,32 +37,22 @@ function App() {
       ignore = true;
     };
   }, []);
-  const getStudents = async () => {
-    try {
-      await fetch(`${API_BASE}/students`)
-        .then((res) => res.json())
-        .then((data) => {
-          setStudents(data);
-        });
-    } catch (err) {
-      setError(error.message || "unexpected error");
-    } finally {
-      setloading(false);
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Students:</h1>
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Students:</h1>
+        {loading && <p>Loading...</p>}
+        {students.length > 0 && (
           <ul>
-            <li>John</li>
-            <li>Paul</li>
-            <li>George</li>
-            <li>Ronny</li>
+            {students.map((student) => (
+              <li key={student._id}>{student.name}</li>
+            ))}
           </ul>
-        </header>
-      </div>
-    );
-  };
+        )}
+      </header>
+    </div>
+  );
 }
 
 export default App;
