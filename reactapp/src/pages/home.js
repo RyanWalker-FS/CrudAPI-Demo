7f10d7d4d419c0b0ddae87fd5e5133027c1231be
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-13 08:55:19 -0500

    home and dash

---
+++ reactapp/src/pages/home.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import "../App.css";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Students List Homepage</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
