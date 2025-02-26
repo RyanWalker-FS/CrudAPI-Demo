2e50d0cbede4414208e2435d2a85c359c8512405
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-25 20:06:39 -0500

    update

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,16 +3,7 @@ import Home from "./pages/home";
 import Dashboard from "./pages/Dashboard";
 import Student from "./pages/student";
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        <Route exact path="/student" element={<Student />} />
-      </Routes>
-    </Router>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
